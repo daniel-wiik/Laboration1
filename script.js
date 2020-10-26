28892f507e363814606360d9eee850252697131b
daniel-wiik <danielwiik89@gmail.com>
2020-10-26 20:54:10 +0100

    added else if and some small changes

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 //start game on click
 function startGame() {
 
-    alert('You have awakened from a fifty year long cryosleep.')
-
-    let firstAction = prompt('You see a button on the cryochamber hatch. What will you do?')
+    alert('You have awakened from a fifty year long cryosleep.');
     
-    if (firstAction === 'press the button') {
-        alert ('The hatch is now opening')
-    }
+    let firstAction = prompt('You see a button on the cryochamber hatch. What will you do?');
     
-    if (firstAction !== 'press the button') {
-        alert ('Wrong command input')
+        if (firstAction === 'press the button') {
+            alert ('The hatch is now opening');
+        }
+        else if (firstAction === 'go back to sleep') {
+              alert ('You are now entering a cryosleep. You will be safe here from the xenomorph until a rescue team arrives.');
+        }
+        else {
+            alert ('Wrong command input');
+        }
+    
     }
-    if (firstAction === 'go back to sleep'); {
-          alert ('You are now entering a cryosleep. You will be safe here from the xenomorph until a rescue team arrives.')
-   
-    }
-}
-
-
