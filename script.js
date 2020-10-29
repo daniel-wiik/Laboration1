1b7f389e35bd3b61e5b3310cc2cb4ad9bc7e2a8c
daniel-wiik <danielwiik89@gmail.com>
2020-10-29 21:55:28 +0100

    Avbryt now quits the game. Upper/lowercase when input command

--- script.js
+++ script.js
@@ -1,47 +1,89 @@
 //start game on click
 function startGame() {
 
-    alert('You have awakened from a fifty year long cryosleep.');
+    let firstScene = alert('You have awakened from a fifty year long cryosleep.');
 
     while (true) {
-        let firstScene = prompt('You see a button on the cryochamber hatch. What will you do?');
+        firstScene = prompt('You see a button on the cryochamber hatch. What will you do?');
 
-        if (firstScene === 'press the button') {
+        if (firstScene === null) {
+            alert('You have quit the game.');
+            return false;
+        }
+
+        let firstResponse = firstScene.toLowerCase();
+
+        if (firstResponse === 'press the button') {
             alert('The hatch is now opening. You are in an unfamiliar spaceship');
             break; //make this action loop if wrong input
         }
-        else if (firstScene === 'go back to sleep') {
-            alert('You are now entering a cryosleep. You will sleep safely until a rescue team arrives.')
-            alert('Reloading checkpoint')
+        else if (firstResponse === 'go back to sleep') {
+            alert('You are now entering a cryosleep. You will sleep safely until a rescue team arrives.');
+            alert("You have unlocked ending 'A'");
+            return false
         }
         else {
             alert("Wrong command input. Try 'press the button' or 'go back to sleep'");
         }
     }
 
-    while (true) {
-        let secondScene = prompt('You see a door that is locked, and there is a keycard next to it. What will you do?');
 
-        if (secondScene === 'pick up keycard') {
+
+    while (true) {
+        secondScene = prompt('You see a door that is locked, and there is a keycard next to it. What will you do?');
+
+        if (secondScene === null) {
+            alert('You have quit the game.');
+            return false;
+        }
+        let secondResult = secondScene.toLowerCase();
+
+        if (secondResult === 'pick up keycard') {
             alert('You have picked up the keycard. You can now open the door.');
             break;
         }
         else {
-            alert("Wrong command input. Try 'pick up keycard'")
+            alert("Wrong command input. Try 'pick up keycard'");
         }
 
     }
     while (true) {
-        let secondScene = confirm('Press OK to unlock the door')
+        secondScene = confirm('Press OK to unlock the door');
 
         if (secondScene == true) {
-            alert('The door is now unlocked.')
+            alert('The door is now unlocked.');
             break;
         }
         else {
-            alert('*crickets chirping*')
+            alert('*crickets chirping*');
+        }
+    }
+
+    let thirdScene = alert('You are in a dark narrow hallway. You can hear heavy steps from above');
+
+    while (true) {
+        thirdScene = prompt('The steps are getting louder. You can either hide inside a locker or keep walking.');
+
+        if (thirdScene === null) {
+            alert('You have the quit the game.');
+            return false;
+        }
+
+        let thirdResponse = thirdScene.toLowerCase();
+
+        if (thirdResponse === 'hide') {
+            alert('You hid inside the locker. You see an alien being walking right past you. It looks dangerous.');
+            alert('The coast is clear now. You leave the locker and keep exploring the spaceship.');
+            break;
+        }
+        else if (thirdResponse === 'keep walking') {
+            alert('The alien being drops out of a vent from above after hearing your footsteps, and it is approaching you.');
+            alert('You have nothing to defend yourself with, so the alien killed you.');
+            alert('Loading checkpoint.');
+        }
+        else {
+            alert("Wrong input command. Try 'keep walking' or 'hide'.");
         }
     }
 }
-
 
